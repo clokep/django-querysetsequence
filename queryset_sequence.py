0b875aeb8aaea20ba47fc2fbc285d078aee42240
Frankie Dintino <fdintino@theatlantic.com>
2015-09-07 10:29:25 -0400

    Initial commit
    From django-ko-demo @ 1a37c9ad9bcd68a40c35462fb819fff85a9533f7.
        https://raw.githubusercontent.com/theatlantic/django-ko-demo/master/apps/curation_nouveau/queryset_sequence.py

--- queryset_sequence.py
+++ queryset_sequence.py
@@ -4,22 +4,20 @@ from operator import mul, attrgetter, __not__
 from django.db.models.query import REPR_OUTPUT_SIZE, EmptyQuerySet
 
 
-
-def mul_it(it1, it2):
-    '''
+def multiply_iterables(it1, it2):
+    """
     Element-wise iterables multiplications.
-    '''
+    """
     assert len(it1) == len(it2),\
            "Can not element-wise multiply iterables of different length."
     return map(mul, it1, it2)
 
 
-
-def chain_sing(*iterables_or_items):
-    '''
+def chain_singleton(*iterables_or_items):
+    """
     As itertools.chain except that if an argument is not iterable then chain it
     as a singleton.
-    '''
+    """
     for iter_or_item in iterables_or_items:
         if hasattr(iter_or_item, '__iter__'):
             for item in iter_or_item:
@@ -28,30 +26,30 @@ def chain_sing(*iterables_or_items):
             yield iter_or_item
 
 
-
-
-
-class IableSequence(object):
-    '''
+class IterableSequence(object):
+    """
     Wrapper for sequence of iterable and indexable by non-negative integers
     objects. That is a sequence of objects which implement __iter__, __len__ and
     __getitem__ for slices, ints and longs.
 
     Note: not a Django-specific class.
-    '''
+    """
+
     def __init__(self, *args, **kwargs):
-        self.iables = args # wrapped sequence
+        self.iterables = args # wrapped sequence
         self._len = None # length cache
         self._collapsed = [] # collapsed elements cache
 
     def __len__(self):
         if not self._len:
-            self._len = sum(len(iable) for iable in self.iables)
+            self._len = sum(len(iterable) for iterable in self.iterables)
         return self._len
 
-
     def __iter__(self):
-        return chain(*self.iables)
+        return chain(*self.iterables)
+
+    def iterator(self):
+        return self
 
     def __nonzero__(self):
         try:
@@ -60,31 +58,30 @@ class IableSequence(object):
             return False
         return True
 
-
     def _collect(self, start=0, stop=None, step=1):
         if not stop:
             stop = len(self)
-        sub_iables = []
+        sub_iterables = []
         # collect sub sets
-        it = self.iables.__iter__()
+        it = self.iterables.__iter__()
         try:
-            while stop>start:
+            while stop > start:
                 i = it.next()
                 i_len = len(i)
                 if i_len > start:
                     # no problem with 'stop' being too big
-                    sub_iables.append(i[start:stop:step])
-                start = max(0, start-i_len)
+                    sub_iterables.append(i[start:stop:step])
+                start = max(0, start - i_len)
                 stop -= i_len
         except StopIteration:
             pass
-        return sub_iables
+        return sub_iterables
 
     def __getitem__(self, key):
-        '''
+        """
         Preserves wrapped indexable sequences.
         Does not support negative indices.
-        '''
+        """
         # params validation
         if not isinstance(key, (slice, int, long)):
             raise TypeError
@@ -95,37 +92,36 @@ class IableSequence(object):
         # initialization
         if isinstance(key, slice):
             start, stop, step = key.indices(len(self))
-            ret_item=False
+            ret_item = False
         else: # isinstance(key, (int,long))
-            start, stop, step = key, key+1, 1
-            ret_item=True
+            start, stop, step = key, key + 1, 1
+            ret_item = True
         # collect sub sets
-        ret_iables = self._collect(start, stop, step)
+        ret_iterables = self._collect(start, stop, step)
         # return the simplest possible answer
-        if not len(ret_iables):
+        if not len(ret_iterables):
             if ret_item:
                 raise IndexError("'%s' index out of range" % self.__class__.__name__)
             return ()
         if ret_item:
             # we have exactly one query set with exactly one item
-            assert len(ret_iables) == 1 and len(ret_iables[0]) == 1
-            return ret_iables[0][0]
+            assert len(ret_iterables) == 1 and len(ret_iterables[0]) == 1
+            return ret_iterables[0][0]
         # otherwise we have more then one item in at least one query set
-        if len(ret_iables) == 1:
-            return ret_iables[0]
-        # Note: this can't be self.__class__ instead of IableSequence; exemplary
+        if len(ret_iterables) == 1:
+            return ret_iterables[0]
+        # Note: this can't be self.__class__ instead of IterableSequence; exemplary
         # cause is that indexing over query sets returns lists so we can not
         # return QuerySetSequence by default. Some type checking enhancement can
         # be implemented in subclasses.
-        return IableSequence(*ret_iables)
-
+        return IterableSequence(*ret_iterables)
 
     def collapse(self, stop=None):
-        '''
+        """
         Collapses sequence into a list.
 
         Try to do it effectively with caching.
-        '''
+        """
         if not stop:
             stop = len(self)
         # if we already calculated sufficient collapse then return it
@@ -133,30 +129,33 @@ class IableSequence(object):
             return self._collapsed[:stop]
         # otherwise collapse only the missing part
         items = self._collapsed
-        sub_iables = self._collect(len(self._collapsed), stop)
-        for sub_iable in sub_iables:
-            items+=sub_iable
+        sub_iterables = self._collect(len(self._collapsed), stop)
+        for sub_iterable in sub_iterables:
+            items += sub_iterable
         # cache new collapsed items
         self._collapsed = items
         return self._collapsed
 
     def __repr__(self):
         # get +1 element for the truncation msg if applicable
-        items = self.collapse(stop=REPR_OUTPUT_SIZE+1)
+        items = self.collapse(stop=REPR_OUTPUT_SIZE + 1)
         if len(items) > REPR_OUTPUT_SIZE:
             items[-1] = "...(remaining elements truncated)..."
         return repr(items)
 
 
-
-class QuerySetSequence(IableSequence):
-    '''
+class QuerySetSequence(IterableSequence):
+    """
     Wrapper for the query sets sequence without the restriction on the identity
     of the base models.
-    '''
+    """
+
+    def all(self):
+        return self
+
     def count(self):
         if not self._len:
-            self._len = sum(qs.count() for qs in self.iables)
+            self._len = sum(qs.count() for qs in self.iterables)
         return self._len
 
     def __len__(self):
@@ -164,9 +163,9 @@ class QuerySetSequence(IableSequence):
         return self.count()
 
     def order_by(self, *field_names):
-        '''
+        """
         Returns a list of the QuerySetSequence items with the ordering changed.
-        '''
+        """
         # construct a comparator function based on the field names prefixes
         reverses = [1] * len(field_names)
         field_names = list(field_names)
@@ -176,13 +175,11 @@ class QuerySetSequence(IableSequence):
                 reverses[i] = -1
                 field_names[i] = field_name[1:]
         # wanna iterable and attrgetter returns single item if 1 arg supplied
-        fields_getter = lambda i: chain_sing(attrgetter(*field_names)(i))
+        fields_getter = lambda i: chain_singleton(attrgetter(*field_names)(i))
         # comparator gets the first non-zero value of the field comparison
         # results taking into account reverse order for fields prefixed with '-'
-        comparator = lambda i1, i2:\
-            dropwhile(__not__,
-              mul_it(map(cmp, fields_getter(i1), fields_getter(i2)), reverses)
-            ).next()
+        comparator = lambda i1, i2: dropwhile(__not__,
+              multiply_iterables(map(cmp, fields_getter(i1), fields_getter(i2)), reverses)).next()
         # return new sorted list
         return sorted(self.collapse(), cmp=comparator)
 
@@ -207,14 +204,14 @@ class QuerySetSequence(IableSequence):
         return self._filter_or_exclude(True, *args, **kwargs)
 
     def _simplify(self, qss=None):
-        '''
+        """
         Returns QuerySetSequence, QuerySet or EmptyQuerySet depending on the
         contents of items, i.e. at least two non empty QuerySets, exactly one
         non empty QuerySet and all empty QuerySets respectively.
 
         Does not modify original QuerySetSequence.
-        '''
-        not_empty_qss = filter(None, qss if qss else self.iables)
+        """
+        not_empty_qss = filter(None, qss if qss else self.iterables)
         if not len(not_empty_qss):
             return EmptyQuerySet()
         if len(not_empty_qss) == 1:
@@ -222,15 +219,15 @@ class QuerySetSequence(IableSequence):
         return QuerySetSequence(*not_empty_qss)
 
     def _filter_or_exclude(self, negate, *args, **kwargs):
-        '''
+        """
         Maps _filter_or_exclude over QuerySet items and simplifies the result.
-        '''
-        # each Query set is cloned separately
+        """
+        # each QuerySet is cloned separately
         return self._simplify(*map(lambda qs:
-            qs._filter_or_exclude(negate, *args, **kwargs), self.iables))
+            qs._filter_or_exclude(negate, *args, **kwargs), self.iterables))
 
     def exists(self):
-        for qs in self.iables:
+        for qs in self.iterables:
             if qs.exists():
                 return True
         return False
