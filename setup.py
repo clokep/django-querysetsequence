6a65e4299c04f0363640038c04b983122d8fbdd9
Stephen DiCato <stephendicato@gmail.com>
2016-01-11 10:55:58 -0500

    Update email address.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Chain together multiple (disparate) QuerySets to treat them as a single QuerySet.',
     long_description=long_description(),
     author='Percipient Networks, LLC',
-    author_email='support@percipientnetworks.com',
+    author_email='support@strongarm.io',
     url='https://github.com/percipient/django-querysetsequence',
     download_url='https://github.com/percipient/django-querysetsequence',
     keywords=['django', 'queryset', 'chain', 'multi', 'multiple', 'iterable'],
