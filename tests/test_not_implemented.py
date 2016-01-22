90f50e8a553fb0162dd138885c270dcc1d35f5f2
Patrick Cloke <clokep@patrick.cloke.us>
2016-01-22 11:17:00 -0500

    Add tests for AttributeErrors.

--- tests/test_not_implemented.py
+++ tests/test_not_implemented.py
@@ -8,7 +8,27 @@ from .models import Article
 class MetaTestGenerator(type):
     """Generate identical test method off of a list of data."""
 
-    NOT_IMPLEMENTED = [
+    ATTRIBUTES = []
+    EXPECTED_EXCEPTION = RuntimeError
+
+    def __new__(meta, name, bases, dct):
+        # Give the test function a separate namespace.
+        def gen_test_func(attr):
+            def test(self):
+                with self.assertRaises(meta.EXPECTED_EXCEPTION):
+                    getattr(self.qss, attr)()
+            test.__doc__ = ("Ensure that NotImplementedError is raised when "
+                            "accessing '%s'" % attr)
+            return test
+
+        for attr in meta.ATTRIBUTES:
+            dct['test_' + attr] = gen_test_func(attr)
+
+        return type.__new__(meta, name, bases, dct)
+
+
+class NotImplementedMeta(MetaTestGenerator):
+    ATTRIBUTES = [
         # Methods that return QuerySets.
         'annotate',
         'reverse',
@@ -42,25 +62,51 @@ class MetaTestGenerator(type):
         'update',
         'delete',
     ]
-
-    def __new__(meta, name, bases, dct):
-        # Give the test function a separate namespace.
-        def gen_test_func(attr):
-            def test(self):
-                self.assertRaises(NotImplementedError, getattr(self.qss, attr))
-            test.__doc__ = ("Ensure that NotImplementedError is raised when "
-                            "accessing '%s'" % attr)
-            return test
-
-        for attr in meta.NOT_IMPLEMENTED:
-            dct['test_' + attr] = gen_test_func(attr)
-
-        return type.__new__(meta, name, bases, dct)
+    EXPECTED_EXCEPTION = NotImplementedError
 
 
 class TestNotImplemented(TestCase):
     """Test methods that are not implemented and should raise NotImplemented."""
-    __metaclass__ = MetaTestGenerator
+    __metaclass__ = NotImplementedMeta
+
+    @classmethod
+    def setUpClass(cls):
+        """Set-up some data to be tested against."""
+        cls.qss = QuerySetSequence(Article.objects.all())
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.qss
+
+
+class AttributeErrorMeta(MetaTestGenerator):
+    ATTRIBUTES = [
+        # Private methods used by not implemented methods.
+        '_populate_pk_values',
+        '_create_object_from_params',
+        '_extract_model_params',
+        '_earliest_or_latest',
+        '_raw_delete',
+        '_update',
+        '_prefetch_related_objects',
+
+        # Private methods.
+        '_insert',
+        '_batched_insert',
+        '_next_is_sticky',
+        '_merge_known_related_objects',
+        '_setup_aggregate_query',
+        '_as_sql',
+        '_add_hints',
+        '_has_filters',
+        'is_compatible_query_object_type',
+    ]
+    EXPECTED_EXCEPTION = AttributeError
+
+
+class TestAttributeError(TestCase):
+    """Test methods that are not implemented and should raise NotImplemented."""
+    __metaclass__ = AttributeErrorMeta
 
     @classmethod
     def setUpClass(cls):
