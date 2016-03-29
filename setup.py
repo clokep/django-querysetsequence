fd5c6c8559f33d184d5d3b8219cb9d8a377730b9
Patrick Cloke <clokep@patrick.cloke.us>
2016-03-29 15:57:37 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def long_description():
 setup(
     name='django-querysetsequence',
     py_modules=['queryset_sequence'],
-    version='0.5',
+    version='0.6',
     description='Chain together multiple (disparate) QuerySets to treat them as a single QuerySet.',
     long_description=long_description(),
     author='Percipient Networks, LLC',
