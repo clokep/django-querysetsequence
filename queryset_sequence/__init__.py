c63039922a069bbed5f2f9dc2aec5cfb8a55b280
Patrick Cloke <clokep@patrick.cloke.us>
2020-07-08 17:15:21 -0400

    Only add the additional parameter in new Djangos.

--- queryset_sequence/__init__.py
+++ queryset_sequence/__init__.py
@@ -571,8 +571,13 @@ class QuerySetSequence(ComparatorMixin):
     def dates(self, field, kind, order='ASC'):
         raise NotImplementedError()
 
-    def datetimes(self, field_name, kind, order='ASC', tzinfo=None, is_dst=None):
-        raise NotImplementedError()
+    # Django 3.1 added an additional parameter.
+    if django.VERSION < (3, 1):
+        def datetimes(self, field_name, kind, order='ASC', tzinfo=None):
+            raise NotImplementedError()
+    else:
+        def datetimes(self, field_name, kind, order='ASC', tzinfo=None, is_dst=None):
+            raise NotImplementedError()
 
     def none(self):
         # This is a bit odd, but use the first QuerySet to properly return an
