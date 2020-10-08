0b2370e7760984d3668f803c2f885672c756d781
Patrick Cloke <clokep@patrick.cloke.us>
2020-10-08 18:00:03 -0400

    Specify the docs entry point.

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,8 @@ release = "0.14"
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
